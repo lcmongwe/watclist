7b79043e68d2c3f6d3845ecb62909a5f629e2bf0
lcmongwe <lcmongwe@gmail.com>
2022-05-04 14:12:25 +0300

    forms.py created

--- app/main/views.py
+++ app/main/views.py
@@ -3,6 +3,7 @@ from . import main
 from ..request import get_movies,get_movie,search_movie
 from ..models import Review
 from .forms import ReviewForm
+from flask_login import login_required
 
 
 
@@ -56,6 +57,7 @@ def search(movie_name):
 
 
 @main.route('/movie/review/new/<int:id>', methods = ['GET','POST'])
+@login_required
 def new_review(id):
     form = ReviewForm()
     movie = get_movie(id)
